fda9a127de4dcf21e193e604d5425ab98129d617
HereToLearnhaha <itsgaurab4@gmail.com>
2024-08-14 12:18:18 +0545

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import models.Mammalia;
 public class Main {
     public static void main(String[] args) {
 
-        Mammalia cow = new Mammalia();
+        Mammalia cow = new Mammalia("cow1", "");
         cow.getHello();
 
-        Mammalia dog = new Mammalia();
+        Mammalia dog = new Mammalia("dog1","");
         dog.getHello();
+
+        System.out.println(cow.type);
+        System.out.println(dog.type);
 
     }
 }
--- src/models/Mammalia.java
+++ src/models/Mammalia.java
@@ -4,7 +4,13 @@ public class Mammalia {
 
     public int noOfChambers = 4;
     public boolean hairy_body =true;
+    public  String name;
+    public String type;
 
+    public Mammalia(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
 
     public void getHello(){
         System.out.println("Hello There!");
